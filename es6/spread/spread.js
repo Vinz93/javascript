96668f5eb3c3cc7e4c471c878a18d8cb32cf05a7
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-06-10 16:10:05 -0400

    add rest operator

--- es6/spread/spread.js
+++ es6/spread/spread.js
@@ -1,16 +1,23 @@
+/*
+ *   Spread Operator 💪
+ */
+
 const ford = ['ka', 'Explorer', 'Fiesta'];
 const toyota = ['meru', 'corolla', 'yaris'];
 
 /* Merge all the brands in a new array */
+
 const all = [...ford, 'aveo', ...toyota];
 const shallCopy = [...all];
 console.log('some cards', shallCopy);
 
 /* works with all iterables */
+
 const name = "Vinz";
 console.log([...name]);
 
 /* imutables delete*/
+
 const comments = [
   { id: 209384, text: 'I love your dog!' },
   { id: 523423, text: 'Cuuute! 🐐' },
